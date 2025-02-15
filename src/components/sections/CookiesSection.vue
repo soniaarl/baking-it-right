d291f3185dd9fe0b20b2ce0bfbdcd8be9155f600
soniaarl <sonia@sparxpg.com>
2025-02-15 01:32:36 -0800

    bug fix (update image file path)

--- src/components/sections/CookiesSection.vue
+++ src/components/sections/CookiesSection.vue
@@ -21,37 +21,37 @@ import CookieCard from '@/components/parts/CookieCard.vue'
 
 const cookies = [
   {
-    image: './public/cookies-images/chocolate-chunk.png',
+    image: './cookies-images/chocolate-chunk.png',
     alt: 'Chocolate chunk cookie',
     name: 'Chocolate Chunk',
     description: 'dark chocolate mixed into our brown butter dough, topped with flaky salt',
   },
   {
-    image: './public/cookies-images/gochujang.png',
+    image: './cookies-images/gochujang.png',
     alt: 'Gochujang',
     name: 'gochujang',
     description: 'our base cookie recipe topped with a gochujang dulce de leche for a mild kick',
   },
   {
-    image: './public/cookies-images/egg-yolk.png',
+    image: './cookies-images/egg-yolk.png',
     alt: 'Salted egg yolk cookie',
     name: 'salted egg yolk',
     description: 'mixed into the dough and sprinkled on top for a rich, umami cookie',
   },
   {
-    image: './public/cookies-images/pineapple.png',
+    image: './cookies-images/pineapple.png',
     alt: 'Pineapple bun cookie',
     name: 'pineapple bun',
     description: 'our base cookie recipe with a buttery, flaky cookie topping',
   },
   {
-    image: './public/cookies-images/hojicha.png',
+    image: './cookies-images/hojicha.png',
     alt: 'Hojicha cookie',
     name: 'hojicha',
     description: 'a deep hojicha cookie topped with caramelized white chocolate',
   },
   {
-    image: './public/cookies-images/pb-miso.png',
+    image: './cookies-images/pb-miso.png',
     alt: 'Peanut butter miso cookie',
     name: 'peanut butter miso',
     description: 'a twist on a classic makes this cookie a perfect balance of salty and sweet',
