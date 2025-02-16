5c227e9cd3e43f537f04db0e24b26c503800d560
soniaarl <sonia@sparxpg.com>
2025-02-16 01:10:49 -0800

    faq section

---
+++ src/components/parts/QuestionCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="qa">
+    <img src="../../../icon-images/milk.png" alt="Glass of milk" />
+    <div class="copy">
+      <h3>{{ question }}</h3>
+      <p>{{ answer }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  question: {
+    type: String,
+    default: 'question',
+  },
+  answer: {
+    type: String,
+    default: 'answer',
+  },
+})
+</script>
+
+<style scoped>
+img {
+  width: 36px;
+  height: fit-content;
+}
+
+.qa {
+  display: flex;
+}
+
+h3 {
+  line-height: 1.1em;
+  margin-bottom: 5px;
+}
+
+.copy {
+  margin-left: 15px;
+}
+</style>
--- src/components/sections/FaqSection.vue
+++ src/components/sections/FaqSection.vue
@@ -24,9 +24,39 @@
         alt="Chocolate chip cookie"
       />
     </div>
-    <div class="faq"></div>
+    <div class="faq wrap" v-for="question in questions" :key="question.question">
+      <QuestionCard :question="question.question" :answer="question.answer" />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import QuestionCard from '../parts/QuestionCard.vue'
+
+const questions = [
+  {
+    question: 'What do the cookies taste like?',
+    answer: 'Delicious',
+  },
+  {
+    question: 'Can I mix and match?',
+    answer: 'Yes, you can pick up to 2 flavours for half a dozen and up to 4 flavours for a dozen.',
+  },
+
+  {
+    question: 'how quickly can i get my cookies?',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+  {
+    question: 'do you take custom orders?',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+  {
+    question: 'what if i have allergies?',
+    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+]
+</script>
 
 <style scoped>
 .cookie-break {
@@ -81,5 +111,8 @@
 .cookie-04 {
   bottom: 10%;
   left: 5%;
+}
+.faq {
+  margin-top: 30px;
 }
 </style>
