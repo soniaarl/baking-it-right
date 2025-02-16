52f2335a5300a2b4baeec9dc8f2b68733fcc1b2c
soniaarl <sonia@sparxpg.com>
2025-02-15 23:03:38 -0800

    refactor (CookieCard props)

--- src/components/parts/CookieCard.vue
+++ src/components/parts/CookieCard.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="CookieCard">
-    <img :src="props.image" :alt="props.alt" />
+    <img :src="image" :alt="alt" />
     <div class="copy">
-      <h3>{{ props.name }}</h3>
-      <p>{{ props.description }}</p>
+      <h3>{{ name }}</h3>
+      <p>{{ description }}</p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue'
-
-interface CookieProps {
-  image: string
-  alt: string
-  name: string
-  description: string
-}
-
-const props = defineProps<CookieProps>()
+defineProps({
+  image: {
+    type: String,
+    default: 'Title',
+  },
+  alt: {
+    type: String,
+    default: 'Alt',
+  },
+  name: {
+    type: String,
+    default: 'Name',
+  },
+  description: {
+    type: String,
+    default: 'Description',
+  },
+})
 </script>
 
 <style scoped>
