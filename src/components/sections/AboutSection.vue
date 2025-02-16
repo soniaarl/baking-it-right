c10505ad7f55a3520906559d77296ab24dc6e41f
soniaarl <sonia@sparxpg.com>
2025-02-16 00:27:11 -0800

    completed AboutSection

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -1,3 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <SectionTitle title="about" colour="yellow" />
+    <div class="wrap">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris
+      </p>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import SectionTitle from '../parts/SectionTitle.vue'
+</script>
+
+<style scoped>
+p {
+  text-align: center;
+}
+</style>
