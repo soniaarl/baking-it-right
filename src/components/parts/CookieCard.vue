fb16e1d6cb44cbce2f313c48395c4b60aec6bcfc
soniaarl <sonia@sparxpg.com>
2025-02-18 01:10:28 -0800

    cookie animation

--- src/components/parts/CookieCard.vue
+++ src/components/parts/CookieCard.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="CookieCard">
-    <img :src="image" :alt="alt" />
+    <img
+      @click="playSound"
+      :class="{ animate: isAnimating }"
+      :src="image"
+      :alt="alt"
+      @animationend="onAnimationEnd"
+    />
     <div class="copy">
       <h3>{{ name }}</h3>
       <p>{{ description }}</p>
@@ -9,6 +15,21 @@
 </template>
 
 <script setup lang="ts">
+import soundFile from '@/assets/bite-noise.mp3'
+import { ref } from 'vue'
+
+const playSound = () => {
+  const audio = new Audio(soundFile)
+  audio.play()
+  isAnimating.value = true
+}
+
+const isAnimating = ref(false)
+
+function onAnimationEnd() {
+  isAnimating.value = false
+}
+
 defineProps({
   image: {
     type: String,
@@ -39,6 +60,11 @@ img {
   margin-right: 18px;
   width: 40%;
   height: fit-content;
+  cursor: pointer;
+}
+
+.animate {
+  animation: shake 0.5s ease;
 }
 
 .copy {
@@ -50,6 +76,44 @@ img {
     display: block;
     text-align: center;
     max-width: 265px;
+  }
+}
+
+@keyframes shake {
+  0% {
+    transform: translate(1px, 1px) rotate(0deg);
+    opacity: 1;
+  }
+  10% {
+    transform: translate(-1px, -2px) rotate(-1deg);
+  }
+  20% {
+    transform: translate(-3px, 0px) rotate(1deg);
+  }
+  30% {
+    transform: translate(3px, 2px) rotate(0deg);
+  }
+  40% {
+    transform: translate(1px, -1px) rotate(1deg);
+  }
+  50% {
+    transform: translate(-1px, 2px) rotate(-1deg);
+  }
+  60% {
+    transform: translate(-3px, 1px) rotate(0deg);
+  }
+  70% {
+    transform: translate(3px, 1px) rotate(-1deg);
+  }
+  80% {
+    transform: translate(-1px, -1px) rotate(1deg);
+  }
+  90% {
+    transform: translate(1px, 2px) rotate(0deg);
+  }
+  100% {
+    transform: translate(1px, -2px) rotate(-1deg);
+    opacity: 0;
   }
 }
 </style>
